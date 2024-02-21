8863fb61aa667b8fd65e3062b37930ebc68c6830
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2024-02-21 19:04:52 +0530

    Time: 3 ms (88.76%), Space: 8.7 MB (46.79%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,14 +1,45 @@
 class Solution {
 public:
-    int rangeBitwiseAnd(int l, int r) {
-       int cnt=0;
-        while(l!=r){
-            l=l>>1;
-            r=r>>1;
-            cnt++;
+    int rangeBitwiseAnd(int left, int right) {
+        int count=0;
+        while(left!=right){
+            left=left>>1;
+            right=right>>1;  
+            count++;
         }
-        return l<<cnt;
+        return left<<count;
     }
 };
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*  5=0101 ,7=0111 so check until both are same  in last you will get 01 by shifting right by 1;
  but your ans is 0100 and you got 01 so mmake cnt and shift it by cnt i.e. l<<cnt or r<<cnt;   */
