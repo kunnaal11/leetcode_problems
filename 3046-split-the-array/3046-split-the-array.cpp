509efcc0496bcc4816f88c88109f31e2a0921c2a
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2024-02-25 15:33:19 +0530

    Time: 16 ms (20.00%), Space: 25.3 MB (20.00%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.cpp
+++ 3046-split-the-array/3046-split-the-array.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
-        // int sum=0;
-        // unordered_map<int,int>map;
-        // for (int i = 0; i < nums.size(); i++) {
-        //     map[nums[i]]= map[nums[i]]+1
-        //         ;
-        //     if(map[nums[i]]>2) return 0;
-        // }
-        // return 1;
+        int sum=0;
+        unordered_map<int,int>map;
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]]= map[nums[i]]+1
+                ;
+            if(map[nums[i]]>2) return 0;
+        }
+        return 1;
         // M-1 logic iss that we need to find if any no is occcuring >2 then it will false
         
         // M-2 sort and if 1st and 3rd same then false else true;
         
-        sort(nums.begin(),nums.end());
+      /*    sort(nums.begin(),nums.end());
         for(int i=2;i<nums.size();i++){
             if(nums[i-2]==nums[i]) return 0;
             
         }
-        return 1;
+        return 1;   */
     }
 };
