a32f66f9f84e6e1e669d1344febfe11e9e681c49
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2024-02-27 23:42:58 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (38.87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -6,13 +6,13 @@ public:
                ans.push_back(output);
                return ;
            }
-           int num=str[i]-'0';
-           string value=mapp[num];
+           int num=str[i]-'0';  //get number which is is str i.e. "23" so got this no 2
+           string value=mapp[num]; //get vaules at that index;
            
-           for(int j=0;j<value.size();j++){
+           for(int j=0;j<value.size();j++){ //split values for each;
                output.push_back(value[j]);
-               solve(str,i+1,ans,output,mapp);
-                output.pop_back();
+               solve(str,i+1,ans,output,mapp);  //recall
+                output.pop_back();         //backtracking
                
            }
        }
@@ -22,7 +22,8 @@ public:
         string output;
         int i=0;
         if(str.size()==0) return {};
-    string mapp[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    string mapp[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};  
+        //will store this in string and get its value by index ;
         solve(str,i,ans,output,mapp);
         return ans;
     }
