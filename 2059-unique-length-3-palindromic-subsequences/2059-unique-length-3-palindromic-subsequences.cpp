04c23944c62879b067d62d2eaff5ee54c1c03d7c
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-01-04 23:56:52 +0530

    Time: 143 ms (85.58%), Space: 15.9 MB (69.02%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string str) {
-        vector<int> first(26, -1);
+        vector<int> first(26, -1);                    //storing first and last occurence of eles
         vector<int> last(26, -1);
 
         for (int i = 0; i < str.size(); i++) {
-            int curr = str[i] - 'a';
-            if (first[curr] == -1) {
-                first[curr] = i;
+            int curr = str[i] - 'a';                //converting into no for index
+            if (first[curr] == -1) { 
+                first[curr] = i;         // first ele gets when its value is -1, and store its index in first array
             } else {
-                last[curr] = i;
+                last[curr] = i;             //if its occure again in string then store after comin in last array
             }
         }
-
+        //find unique char between first and last occrence letter
         int ans = 0;
         for (int i = 0; i < 26; i++) {
-            if (last[i] == -1) {
+            if (last[i] == -1) {            //Skip characters not present in the string
                 continue;
-            } else {
+            } else {           //unique char between first and last 
 
                 unordered_set<char> st;
                 for (int j = first[i] + 1; j < last[i]; j++) {
                     st.insert(str[j]);
                 }
-                ans += st.size();
+                ans = ans + st.size();       //ans update
             }
         }
         return ans;
