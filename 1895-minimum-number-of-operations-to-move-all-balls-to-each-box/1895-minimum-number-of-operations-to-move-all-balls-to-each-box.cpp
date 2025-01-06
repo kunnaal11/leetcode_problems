075719a0936d71ed20de6dcfecb20ec32c41e25e
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-01-06 15:48:54 +0530

    Time: 0 ms (100%), Space: 12.7 MB (8.62%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int n=boxes.size();
-        vector<int>ans(n);
+        int n=boxes.size();                               //M1-bruth force
+     /*   vector<int>ans(n);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j && boxes[j]=='1'){
@@ -10,6 +10,28 @@ public:
                 }
             }
         }
-        return ans;
+        return ans;     */
+                                                         //M2- prefix traversal ,suffix travel
+        vector<int>ans(n);
+        vector<int>left(n);
+        vector<int>right(n);
+
+        int balls=boxes[0]-'0';
+        for(int i=1;i<n;i++){
+             left[i]=left[i-1]+balls;
+             balls+=boxes[i]-'0';
+        }
+
+         balls=boxes[n-1]-'0';
+        for(int i=n-2;i>=0;i--){
+             right[i]=right[i+1]+balls;
+             balls+=boxes[i]-'0';
+        }
+
+        for(int i=0;i<n;i++){
+            ans[i]=left[i]+right[i];
+        }
+    
+    return ans;
     }
 };
