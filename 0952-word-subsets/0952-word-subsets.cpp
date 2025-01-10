db3706b04cb411a6f3f039e61082047e8f075447
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-01-10 23:55:08 +0530

    Time: 54 ms (62.1%), Space: 106.8 MB (49.77%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<int> maxcnt(26, 0);
+        for (auto word : words2) {
+            vector<int> cnt2(26, 0);
+            for (char c : word) {
+                cnt2[c - 'a']++;
+            }
+            for (int i = 0; i < 26; ++i) {
+                maxcnt[i] = max(maxcnt[i], cnt2[i]);
+            }
+        }
+        vector<string> ans;
+        for (auto word : words1) {
+            vector<int> cnt1(26, 0);
+            for (char c : word) {
+                cnt1[c - 'a']++;
+            }
+            bool flag=true;
+            for(int i=0;i<26;i++){
+                if(cnt1[i]<maxcnt[i]) 
+                flag=false;
+            }
+            if(flag){
+                ans.push_back(word);
+            }
+        }
+
+        return ans;
+    }
+
+private:
+    bool isUniversal(const vector<int>& wordFreq, const vector<int>& maxFreq) {
+        for (int i = 0; i < 26; ++i) {
+            if (wordFreq[i] < maxFreq[i])
+                return false;
+        }
+        return true;
+    }
+};
