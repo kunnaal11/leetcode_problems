f686fe4f7c0f121ce02d634c59287417e744b09f
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-02-02 12:27:00 +0530

    Time: 0 ms (100%), Space: 11.1 MB (58.89%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n=nums.size();
-        int cnt=0;
-         for(int i=1;i<n;i++){
-             if(nums[i-1]>nums[i])   
-                 cnt++;
-         }
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1)%n] )      //checking pick occurs >1 for its next ele
+                cnt++;
+        }
+
         
-        if(nums[n-1]>nums[0]) cnt++;
-        
-        return cnt <=1;    //if cnt is 0 ot 1 then its cyclic if >=2 then non cyclic
-    } 
+
+        return cnt <= 1; // if cnt is 0 ot 1 then its cyclic if >=2 then non cyclic
+    }
 };
