89e0485f52d6ec4449f50160605aed1c25872192
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-02-10 15:06:00 +0530

    Time: 3 ms (15.28%), Space: 9.3 MB (14.68%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+       for (char ch : s) {
+            if (isdigit(ch)) {                          // Check if the character is a digit
+                if (!st.empty()) {
+                    st.pop();                          // Remove the last non-digit character
+                }
+            } else {
+                st.push(ch);                           // Push only non-digit characters
+            }
+        }
+        
+        string ans = "";
+        while (!st.empty()) {
+            ans = st.top() + ans;                     // Build the string correctly
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
