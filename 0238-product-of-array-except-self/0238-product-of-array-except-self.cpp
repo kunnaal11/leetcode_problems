50a35f92c35ac769ffdde68c27ee5de89bdc97f2
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-03-24 19:44:36 +0530

    Time: 4 ms (22.11%), Space: 42.1 MB (25.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-         vector<int> left(n,1);
-         vector<int> right(n,1);
-        
-        left[0]=1;      // for understanding that will start with 1st indexx
-        for(int i=1;i<n;i++){
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+
+        left[0]=1;
+       for(int i=1;i<n;i++){
             left[i]=left[i-1]*nums[i-1];
-           
-        }
-        
-        right[n-1]=1;    //its last right ele
+        } 
+
+        right[n-1]=1;
         for(int i=n-2;i>=0;i--){
             right[i]=right[i+1]*nums[i+1];
-           
-        }
+        } 
         
-        vector<int>ans(n);
+        vector<int> ans(n,0);
         for(int i=0;i<n;i++){
             ans[i]=left[i]*right[i];
-           
         }
-    
-        return ans;
+
+      return ans;
     }
-};// approch ans= left * [i] * right;
-//will find all left mul of i 
-//will find all right mul of i 
-//and final multiple it ans
-// note for 1st ele there is no left so start from 1
-//and for right 2nd last
+};
