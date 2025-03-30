883b94d05172df4b9a782799c75035bc9f6bab3b
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-03-30 21:52:30 +0530

    Time: 0 ms (100%), Space: 8.9 MB (49.45%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> lastIndex(26,0);
+
+        for(int i=0;i<s.size();i++){
+            int ch=s[i]-'a';
+            lastIndex[ch]=i;                 //it will store last index of each ele 
+        }
+
+        vector<int>ans;
+        int start=0,end=0;
+        
+        for(int i=0;i<s.size();i++){
+            int ch = s[i]-'a';
+             end=max(end,lastIndex[ch]);
+             if(i==end){                         //if curr = end then we found partition
+                ans.push_back(end-start+1);
+                start=i+1;
+             }                       
+         }
+
+        return ans;
+    }
+};
