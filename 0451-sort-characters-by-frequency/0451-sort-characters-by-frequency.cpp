2a376cfa2da88b996339c6d5b9f3955afbbbb17a
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-04-22 13:28:35 +0530

    Time: 5 ms (29.31%), Space: 10.7 MB (91.11%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,4 +1,8 @@
 class Solution {
+public: 
+   static bool compare(pair<char, int> a, pair<char, int> b) {
+        return a.second > b.second;                   // sort by frequency (high to low)
+}
 public:
     string frequencySort(string s) {
         unordered_map<char, int> mp;
@@ -11,14 +15,13 @@ public:
             pp.push_back({it.first, it.second});
         }
 
-        sort(pp.begin(), pp.end(), [](pair<char,int>& a, pair<char,int>& b){
-            return a.second > b.second;
-        });
+        sort(pp.begin(), pp.end(), compare);    // use the function to sort
 
         string ans = "";
         for (int i = 0; i < pp.size(); i++) {
-            while (pp[i].second-- > 0) {
+            while (pp[i].second > 0) {
                 ans += pp[i].first;
+                pp[i].second--;
             }
         }
 
