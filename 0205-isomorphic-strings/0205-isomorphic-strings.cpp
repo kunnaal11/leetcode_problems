cb64a5da3b3529e8a75a756b519a0efb3c520aee
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-04-22 11:40:21 +0530

    Time: 3 ms (43.63%), Space: 9.3 MB (55.79%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> indexS(128, 0);
-        vector<int> indexT(128, 0);
-        int len=s.size();
-        if(len!=t.size()) return false;
-        
-        for(int i=0;i<len;i++){
-            if(indexS[s[i]]!=indexT[t[i]])
+        unordered_map<char, int> charIndexS;
+        unordered_map<char, int> charIndexT;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (charIndexS.find(s[i]) == charIndexS.end()) {
+                charIndexS[s[i]] = i;
+            }
+
+            if (charIndexT.find(t[i]) == charIndexT.end()) {
+                charIndexT[t[i]] = i;
+            }
+
+            if (charIndexS[s[i]] != charIndexT[t[i]]) {
                 return false;
-            indexS[s[i]]=i+1;
-            indexT[t[i]]=i+1;
+            }
         }
-        return true;
+
+        return true;        
     }
 };
