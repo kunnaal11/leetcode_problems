694a7380e918f452a9353d1ae9c14804196672cc
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-05-19 20:48:12 +0530

    Time: 0 ms (100%), Space: 22.9 MB (65.15%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        int maxEle=nums[0];
+        int Tsum=nums[0];
+        bool check=true;
+        for(int i=1;i<3;i++){
+            Tsum+=nums[i];
+            if(nums[i]>maxEle){
+                maxEle=nums[i];
+                
+            }
+        }
+        if(maxEle >= Tsum-maxEle) return "none";
+
+        if(nums[0]==nums[1] && nums[1]==nums[2] ) return "equilateral";
+        else if( (nums[0]==nums[1] && nums[1]!=nums[2]) || (nums[1]==nums[2] && nums[1]!=nums[0]) || (nums[0]==nums[2] && nums[0]!=nums[1])) return "isosceles";
+        else if( maxEle < Tsum-maxEle ) return "scalene";
+        else return "none";
+    }
+};
