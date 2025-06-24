30c7af83005f0ab3a8cc182086f486c8197f65c4
kunnaal11 <111784423+kunnaal11@users.noreply.github.com>
2025-06-24 20:31:17 +0530

    Time: 0 ms (100%), Space: 26.1 MB (66.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ele=0;
-        for(auto it:nums){
-            ele^=it;
+        int n=nums.size();
+        
+        if(n==1) return nums[0];               //edge cases*
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        
+
+        int left=1,right=n-2;
+        while(left <= right){
+
+            int mid = left +(right-left)/2;
+
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];                                    //mil gaya
+            }
+
+            if( (mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1]) ){
+                left=mid+1;                                //rule follows-->means ele will be in right part
+            }
+            else{     //if rule not follow-->occurs in left
+               right=mid-1;
+            }
         }
-        return ele;
+        return -1;
     }
 };
+
+
+//     if mid is even, then it's duplicate should be in next index.
+// first ele alway=even indx,second=odd indx.-->if its follow this then ,ele is i right ,othervise its in left
